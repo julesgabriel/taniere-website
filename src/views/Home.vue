c654b2b42df9c4cabcb890a6dd35cc4dd3ada3dd
Adel Khiter <khiter.adel@gmail.com>
2021-09-15 20:11:30 +0200

    INIT backgroundHome component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="home">
-
+      <div class="background">
+        <BackgroundHome/>
+      </div>
     </div>
 </template>
 
 <script>
 
+import BackgroundHome from "@/components/backgroundHome.vue";
+
+
 export default {
   name: 'Home',
   components: {
+    BackgroundHome,
   }
 }
 </script>
