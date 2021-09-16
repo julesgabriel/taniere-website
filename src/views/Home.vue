1aa2ccb550820fc2aeeeb2e321f7d769b3d6dbb4
Adel Khiter <khiter.adel@gmail.com>
2021-09-16 17:03:38 +0200

    UPDATE home bg & title + responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="home">
-      <div class="background">
-        <BackgroundHome/>
+  <div class="home">
+    <div class="background">
+      <BackgroundHome/>
+      <div class="top-content">
+        <h1 class="title">iimpact</h1>
+        <p class="description">Club école de l'IIM</p>
+      </div>
+      <div class="scrollDown">
+        <div class="mouse">
+          <span></span>
+        </div>
+        <p>scroll down</p>
       </div>
     </div>
+    <div class="content">
+
+    </div>
+  </div>
 </template>
 
 <script>
